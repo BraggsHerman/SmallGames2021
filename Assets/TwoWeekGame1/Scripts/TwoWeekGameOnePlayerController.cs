34d38385c3a93a196d1ca9c3cf280204b8d7ef4e
Herman Braggs <42748017+BraggsHerman@users.noreply.github.com>
2021-03-11 17:49:43 -0600

    worked on animations for character, still have to figure out transition when jumping

--- Assets/TwoWeekGame1/Scripts/TwoWeekGameOnePlayerController.cs
+++ Assets/TwoWeekGame1/Scripts/TwoWeekGameOnePlayerController.cs
@@ -7,10 +7,15 @@ public class TwoWeekGameOnePlayerController : MonoBehaviour
     public int jumpsLeft;
     Rigidbody2D rb;
     public int speed;
+    public Animator anim;
+
+    private SpriteRenderer sprite;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,12 +25,30 @@ public class TwoWeekGameOnePlayerController : MonoBehaviour
         Vector2 position = transform.position;
         position.x = position.x + speed * horitzontal * Time.deltaTime;
         transform.position = position;
+        Debug.Log(horitzontal);
+        
+        anim.SetFloat("Speed", Mathf.Abs(horitzontal));
+
+        if(horitzontal < 0)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (jumpsLeft > 0)
             {
                 jumpsLeft--;
                 rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+                
+                //for jumping, if time permits, come back and set  up layermasks so that jumping animation works
+                //anim.SetBool("IsJumping", true);
+
             }
             if (jumpsLeft <= 0)
             {
